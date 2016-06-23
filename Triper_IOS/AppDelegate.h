09d9cc57a36ff2695cb5c5c90b7f200fd1a14289
Zvigun Alexandr <mircamnia@ukr.net>
2016-06-23 17:00:46 +0300

    Initial Commit

---
+++ Triper_IOS/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Triper_IOS
+//
+//  Created by Mac on 23.06.16.
+//  Copyright © 2016 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Triper_IOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Triper_IOS
+//
+//  Created by Mac on 23.06.16.
+//  Copyright © 2016 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
